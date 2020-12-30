7b06d8f3817630567fd605c33c34b8ca123acfb9
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2020-12-30 17:38:47 -0500

    Clusters and Flask

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+#Import needed stuff
 from flask import Flask
+from flask_pymongo import PyMongo
+from os import environ
 
+#Build App
 app = Flask(__name__)
+
+#Configure
+app.config['MONGO_URI'] = environ.get('MongoDB_URL', 'mongodb://localhost:27017/Aliens2')
+
+#Initalize Mongo CLient
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def index():
